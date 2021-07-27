ff783357750bdfab113ee7115faf32ce3b6c66b6
Mostafa <khoshabi.mostafa@gmail.com>
2021-07-27 14:38:01 +0430

    2

--- Task1.py
+++ Task1.py
@@ -2,25 +2,25 @@
 import numpy as np
 from sklearn import datasets
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import string
 import random
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,Ridge,BayesianRidge
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
 from sklearn.svm import SVR
 
 from sklearn.model_selection import KFold, cross_val_score, train_test_split,GridSearchCV
 import sklearn.metrics as metrics
 
+
+
 #%%
 # *********Task1: Generate Random Dataset******************
 
 # Sample Count
-
-
 n_sample = 1000
 
 # Generate Random Numeric Columns
@@ -32,7 +32,7 @@ _X, _y= datasets.make_regression(n_samples=n_sample,#number of samples
 alphabet = list(string.ascii_lowercase)
 letter_col = random.choices(alphabet,k=n_sample)
 
-# Data Set
+# Final Sample Data Set
 df = pd.concat([pd.DataFrame(_X,columns=["est_num1","est_num2"])
                    ,pd.DataFrame(letter_col,columns=["est_cat"])
                    ,pd.DataFrame(_y,columns=["target"])]
@@ -40,26 +40,36 @@ df = pd.concat([pd.DataFrame(_X,columns=["est_num1","est_num2"])
 
 #%%
 
-# Preprocess
+# Preprocessing
 # Convert Categorical Column to Numeric Columns with One-Hot Encoding
 df["est_cat"] = df["est_cat"].astype(pd.CategoricalDtype(categories=alphabet))
 df = pd.get_dummies(df)
 
-# X , y
+# Define X as estimators and y as target
 X = df.loc[:, df.columns != 'target'].values
 y = df['target'].values
 
-# Split train test data
+# Split Data to train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
+# GETTING Correlation matrix
+corr_mat=df.loc[:, df.columns != 'target'].corr(method='pearson')
+plt.figure(figsize=(20,10))
+sns.heatmap(corr_mat,vmax=1,square=True,annot=True,cmap='cubehelix')
+plt.show()
+
+# !! there is any correlation in estimators
+
 #%%
-# Find Best Model
+# Find the best model
+
 # Check Modeling
 models = []
-models.append(('LR', LinearRegression()))
-models.append(('NN', MLPRegressor(solver='lbfgs')))  # neural network
+models.append(('LinearRegression', LinearRegression()))
+models.append(('Ridge', Ridge()))
+models.append(('BayesianRidge', BayesianRidge()))
 models.append(('KNN', KNeighborsRegressor()))
-models.append(('RF', RandomForestRegressor(n_estimators=10)))  # Ensemble method - collection of many decision trees
+models.append(('RandomForest', RandomForestRegressor(n_estimators=10)))  # Ensemble method - collection of many decision trees
 models.append(('SVR', SVR(gamma='auto')))  # kernel = linear
 
 # Evaluate each model in turn
@@ -74,21 +84,22 @@ for name, model in models:
 
     results.append(cv_results)
     names.append(name)
+    print("***Models Evaluations: R2_mean(std)")
     print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
 
 # Compare Algorithms
+plt.figure()
 plt.boxplot(results, labels=names)
-plt.title('Algorithm Comparison')
+plt.title('Algorithm Comparison (R2)')
 plt.show()
 
 #%%
-# LinearRegression and RandomForest perform equally well.
-# But I personally prefer RF since this ensemble model (combine multiple ‘individual’ (diverse) models together
-# and deliver superior prediction power.)
-# can almost work out of the box and that is one reason why they are very popular.
+# !! LinearRegression, Rigide and RandomForest perform equally well.
+# !! But I personally prefer RandomForestRegression since this ensemble model (combine multiple ‘individual’ (diverse) models together
+# !! and deliver superior prediction power.)
+# !! can almost work out of the box and that is one reason why they are very popular.
 
-# Grid Searching Hyperparameters to find best RandomForest Model
-
+# Grid Searching Hyperparameters to find the best RandomForest Model
 model = RandomForestRegressor()
 param_search = {
     'n_estimators': [20, 50, 100],
@@ -101,10 +112,10 @@ gsearch.fit(X_train, y_train)
 best_score = gsearch.best_score_
 best_model = gsearch.best_estimator_
 
-print("Best Model is RandomForestRegressor with:")
-print("n_estimators: " , best_model.n_estimators )
-print("max_features: " , best_model.max_features)
-print("max_depth   : " , best_model.max_depth)
+print("\n****Best Model is RandomForestRegressor with this parameters:")
+print("    n_estimators: " , best_model.n_estimators )
+print("    max_features: " , best_model.max_features)
+print("    max_depth   : " , best_model.max_depth)
 
 
 #%%
@@ -120,10 +131,10 @@ mse=metrics.mean_squared_error(y_true, y_pred)
 median_absolute_error=metrics.median_absolute_error(y_true, y_pred)
 r2=metrics.r2_score(y_true, y_pred)
 
-print('explained_variance: ', round(explained_variance,4))
-print('r2: ', round(r2,4))
-print('MAE: ', round(mean_absolute_error,4))
-print('MSE: ', round(mse,4))
-print('RMSE: ', round(np.sqrt(mse),4))
+print("\n****Best Model Performance****")
+print('   explained_variance: ', round(explained_variance,4))
+print('   r2: ', round(r2,4))
+print('   MAE: ', round(mean_absolute_error,4))
+print('   MSE: ', round(mse,4))
+print('   RMSE: ', round(np.sqrt(mse),4))
 
-#%%
